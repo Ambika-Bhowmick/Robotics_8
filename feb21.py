28eed4f7d0f9e97240d0aeebcb43f6d242fa838b
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-02-21 10:06:28 -0700

    Create feb21.py

---
+++ feb21.py
@@ -0,0 +1,8 @@
+# I used the lesson from yesterday to make a story that the user has to fill out. I ask the user a bunch of questions and they answer it. Then I printed all of the variable and test together.
+print("""Welcome to your regular day simulator! I am going to ask you a bunch of questions and then create a story with you as the main character""")
+myHobby = input("What is your hobby: ")
+myFavouritefood = input("What is your favourite food: ")
+myFavouriteanimal = input("What is your favourite animal ")
+myName = input("What is your name? ")
+print()
+print("Hey" +  myName  + "! Today you are going to be eating"  + myFavouritefood  + " while doing"  +  myHobby  +  " and then you are going to see a"  +  myFavouriteanimal  +  "!")
