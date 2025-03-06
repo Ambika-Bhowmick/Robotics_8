39959168edcdefbd3f39f66c17a8af35eee80f79
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-03-06 09:05:33 -0700

    Create mar06.py

---
+++ mar06.py
@@ -0,0 +1,26 @@
+# Today I reviewed how to make if statements in python, also I used variables.
+print("THE CHARACTER GENERATOR")
+iceMan = input("Are you made ice? ")
+if iceMan == "yes":
+  print("Then, you're Iceman")
+else:
+  print("Then, you're not Iceman")
+print()
+hulk = input("Are you green? ")
+if hulk == "yes":
+  print("Then, you're Hulk")
+else:
+  print("Then, you're not Hulk")
+print()
+spiderMan = input("Were you bitten by a spider? ")
+if spiderMan == "yes":
+  print("Then, you're Spiderman")
+else:
+  print("Then, you're not Spiderman")
+print()
+ironman = input("Do you have a yellow or red suit? ")
+if ironman == "yes":
+  print("Then, you're Iron man")
+else:
+  print("I guess you're not from Marvel" )
+  
