30f7318c82e35765f52e4193c3c4bb0a38a332be
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-03-11 13:59:22 -0600

    Create mar04.py

---
+++ mar04.py
@@ -0,0 +1,14 @@
+# Day 6
+# We learned how to use elif statments to make a password system.
+print("Secure Login")
+username = input("What is your username?")
+password = input("What is your password?")
+if username == "Sanvi" and password == "Billu rules":
+  print("Welcome, Sanvi! You are looking nice today!")
+elif username == "Sam" and password == "IM AMAZING":
+  print("Hi, Sam! Love your hair today!")
+elif username == "Ambika" and password == "idk what my password should be":
+  print("Hi, Ambika!")
+else: 
+  print("I don't know you. Go away!")
+  # It worked in Replit
