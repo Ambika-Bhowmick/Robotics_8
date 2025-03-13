6068241d391ebf19d7aefbcc7cd67a60e3f69291
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-03-13 09:07:11 -0600

    Create mar13.py

---
+++ mar13.py
@@ -0,0 +1,20 @@
+# Replit Day 7
+# We learned how to properly syntax if statements to make sure there are no errors.
+print("Are you a superfan of Sesame Street or are you fake?")
+Rocco = input("Who is Rocco?")
+if Rocco == "He's Zoe's pet rock!":
+  print("Correct!")
+else:
+  print("Wrong!")
+Zoe = input("Who is Zoe?")
+if Zoe == "She's Rocco's best friend!":
+  print("Correct")
+else:
+  print("Wrong!")
+print("Last question!")
+Elmo = input("Who is Elmo?")
+if Elmo == ("He's the main character who hates Rocco!"):
+  print("Correct!")
+else:
+  print("You're a fake fan!")
+print("Thanks for playing!")
