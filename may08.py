f0c825ac3039dcfec2233d81c7cb9e624118a983
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-05-08 09:04:06 -0600

    Create may08.py

---
+++ may08.py
@@ -0,0 +1,13 @@
+# I learned how to use the int and float to make sure the computer can recognize numbers
+# Day 8
+year = int(input("What year were you born in?"))
+if year >= 1925 and 1946:
+  print("You are a Traditionalist.")
+elif year >= 1947 and 1964:
+  print("You are a Baby Boomer.")
+elif year >= 1965 and 1981:
+  print("You are a Generation X.")
+elif year >= 1982 and 1995:
+  print("You are a Millenial.")
+elif year >= 1996 and 2015:
+  print("You are a Generation Z.")
